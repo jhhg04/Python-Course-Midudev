7a9c805355a443d711ef0ca7cadce4c99107e2e4
jhhg820 <jhhg04@gmail.com>
2026-02-17 09:20:48 -0500

    print() function

--- 01_basic/01_print.py
+++ 01_basic/01_print.py
@@ -3,3 +3,14 @@ print("Hello World")
 print("Hello 'World'")
 print('Hello World')
 print("Phyton", "is", "Awesome")
+
+# 01- print() -> separate -> sep
+print("Phyton", "is", "Brutal", sep="-")
+
+# 01- print() -> line break -> end
+print("This is Printed", end=" ")
+print("on one line")
+
+# 01- print() -> numbers
+print(43)
+print(42.8)
