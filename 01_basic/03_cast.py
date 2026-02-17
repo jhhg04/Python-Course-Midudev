b4451e55aab234b015a778f0877e20fc7d73756b
jhhg820 <jhhg04@gmail.com>
2026-02-17 14:03:42 -0500

    create cast file

---
+++ 01_basic/03_cast.py
@@ -0,0 +1,9 @@
+# 03 casting types
+# Transform type of a value to other
+
+print("type conversion")
+
+# print("100" + 2) # Error
+# print(100 + "2") # Error
+print("100" + str(2))
+print(2 + int("100"))
