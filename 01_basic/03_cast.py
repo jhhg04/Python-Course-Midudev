1deb92598a5e6328046b31d77cf7319a65a582ae
jhhg820 <jhhg04@gmail.com>
2026-02-18 19:29:36 -0500

    create variables

--- 01_basic/03_cast.py
+++ 01_basic/03_cast.py
@@ -7,3 +7,21 @@ print("type conversion")
 # print(100 + "2") # Error
 print("100" + str(2))
 print(2 + int("100"))
+
+# float to int
+print(int(3.1416)) # remove decimal part
+
+# int to bool
+print(bool(0)) # only 0 is false
+print(bool(1))
+print(bool(-1))
+
+# str to bool
+print(bool("")) # only "" empty string is false
+print(bool(" "))
+print(bool("False"))
+
+# round int, to the more close even->par, if is .5
+print(round(2.5))  # 2
+print(round(2.51))  # 3
+print(round(3.5))  # 4
---
+++ 01_basic/04_variables.py
@@ -0,0 +1,3 @@
+my_name = "John"
+
+print(my_name)
