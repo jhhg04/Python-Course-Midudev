55418f3e879390b3bd29d3d0ba6b99b438bb3807
jhhg820 <jhhg04@gmail.com>
2026-02-23 13:33:07 -0500

    add

---
+++ 05_input.py
@@ -0,0 +1,17 @@
+# function input() allows obtain user data through the console.
+
+print("Hi, What is your name?")
+name = input()
+
+print(f" Hi {name}, nice to meet you")
+
+# without print is in sema line, must usar \n
+age = input("Hi, How old are you?\n")
+age = int(age) # if dont use this cast, shoe error because age is string
+print(f"in five years, you will have {age + 5}, very nice")
+
+# get several values in same time
+print("get several values in same time")
+country, city = input("Where country an city do you live?\n").split()
+
+print(f"You live in {country}, {city}")
