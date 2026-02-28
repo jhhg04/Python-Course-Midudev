1091554bda2584c0d02b1797cf3ac3aee35478f0
jhhg820 <jhhg04@gmail.com>
2026-02-27 19:29:24 -0500

    add range list

--- 02_flow_control/03_list.py
+++ 02_flow_control/03_list.py
@@ -47,7 +47,7 @@ print(list1[::2])   # [1, 3, 5, 7] ,return elements at even indices
 print(list1[::-1])  # [8, 7, 6, 5, 4, 3, 2, 1] return the list in reverse order
 
 # Modify a list
-list1[0] = 20
+list1[0] = 20 # Only exits positions 
 print(list1)
 
 # Add elements to a list
