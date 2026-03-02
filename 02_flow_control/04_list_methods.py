06113a5511d001e8ae0e1ba7f02b4f258eb76b37
jhhg820 <jhhg04@gmail.com>
2026-03-02 18:39:32 -0500

    add at the end of list

--- 02_flow_control/04_list_methods.py
+++ 02_flow_control/04_list_methods.py
@@ -1,7 +1,14 @@
 ###
-# 04 - Listas Métodos
-# Los métodos más importantes para trabajar con listas
+# 04 - List Methods
+# The most important methods for working with lists
 ###
 
 from os import system
 if system("clear") != 0: system("cls")
+
+# Create a list with values
+lista1 = ['a', 'b', 'c', 'd']
+
+# Add or insert elements into the list
+lista1.append('e')  # Adds an element to the end
+print(lista1)
