9c79e5b02f2ea29e918072e3011eb4a52d7b1d7c
jhhg440 <jhhg04@gmail.com>
2026-03-06 21:52:32 -0500

    create loop for file

---
+++ 03_loops/02_loop_for.py
@@ -0,0 +1,9 @@
+###
+# 02 - Loops (for)
+# They allow you to execute a block of code repeatedly while iterating over an iterable or a list
+###
+
+from os import system
+if system("clear") != 0: system("cls")
+
+print("\nFor loop:")
