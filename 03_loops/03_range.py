531e40b02d6c9c34be2b105d8bc1c73292a27570
jhhg820 <jhhg04@gmail.com>
2026-03-09 18:20:34 -0500

    range - Generate a sequence of numbers from 0 to 9

---
+++ 03_loops/03_range.py
@@ -0,0 +1,13 @@
+###
+# 03 - range()
+# It allows you to create a sequence of numbers. It can be useful for for-loops, but not only for that
+###
+
+from os import system
+if system("clear") != 0: system("cls")
+
+print("\nrange():")
+
+# Generate a sequence of numbers from 0 to 9
+for num in range(10):
+    print(num)
