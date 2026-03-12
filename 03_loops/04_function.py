5695f3ef7ff5e652a2164c4a6a3aa31e2a84cf82
jhhg820 <jhhg04@gmail.com>
2026-03-11 19:05:52 -0500

    create function file

---
+++ 03_loops/04_function.py
@@ -0,0 +1,15 @@
+# 04 - Functions
+# Reusable and parameterized blocks of code to perform specific tasks
+###
+
+from os import system
+if system("clear") != 0: system("cls")
+
+# """ Function definition
+
+# def function_name(parameter1, parameter2, ...):
+#   # docstring
+#   # function body
+#   return return_value  # optional
+
+# """
