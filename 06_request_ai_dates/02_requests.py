a7e2e1620f3fbe8112cdb0c41c3858ae6c41e764
jhhg440 <jhhg04@gmail.com>
2026-04-06 21:33:33 -0500

    PUT: All object

--- 06_request_ai_dates/02_requests.py
+++ 06_request_ai_dates/02_requests.py
@@ -53,7 +53,7 @@ try:
       "userId": 1,
     })
 
-  print(response.status_code)
+  print(response.status_code) # PUT: All object
 except requests.exceptions.RequestException as e:
   print(f"Request error: {e}")
 
