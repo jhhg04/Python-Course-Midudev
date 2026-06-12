5a4574421484517bb74908cfdc79376fc5bd3e03
jhhg820 <jhhg04@gmail.com>
2026-06-12 17:14:05 -0500

    add space for order

--- 08_log_monitor/monitor.py
+++ 08_log_monitor/monitor.py
@@ -111,6 +111,7 @@ def print_summary(errors, error_summary, ips):
     print(f"\nScan executed at: {current_time}")
     
     if len(errors) == 0:
+        
         print("No issues found")
     else:
         print(f"Total issues found: {len(errors)}")
