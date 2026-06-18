550e5826b67b15d1442b22cdc6d3032e6a1bf0fb
jhhg820 <jhhg04@gmail.com>
2026-06-18 18:15:26 -0500

    add comment results

--- 09_folders/repo_size_analyzer.py
+++ 09_folders/repo_size_analyzer.py
@@ -3,5 +3,6 @@ from pathlib import Path
 # Repository path to analyze
 REPO_PATH = Path(r"C:\Repos\MyRepository" ) # Update this path
 
+# Number of results to display
 TOP_FILES = 50
 TOP_FOLDERS = 30    
