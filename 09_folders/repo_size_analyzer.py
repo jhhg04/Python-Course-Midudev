5ee3f72e2bd166a6d263707a2b259512e099dd3f
jhhg820 <jhhg04@gmail.com>
2026-06-22 19:10:15 -0500

    delete files to evoid

--- 09_folders/repo_size_analyzer.py
+++ 09_folders/repo_size_analyzer.py
@@ -7,15 +7,3 @@ REPO_PATH = Path(r"C:\Repos\MyRepository" ) # Update this path
 TOP_FILES = 50
 TOP_FOLDERS = 30
 
-# Set to True if you want to analyze the .git directory
-INCLUDE_GIT = True
-
-# Directories to exclude from the analysis
-EXCLUDED_DIRS = {
-
-
-
-
-
-
-}
