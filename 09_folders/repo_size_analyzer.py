bae66970c5c83a5efd23aaf0cb017ca1e2ad013d
jhhg820 <jhhg04@gmail.com>
2026-07-03 17:42:59 -0500

    add if current_folder

--- 09_folders/repo_size_analyzer.py
+++ 09_folders/repo_size_analyzer.py
@@ -53,4 +53,5 @@ for file_path in REPO_PATH.rglob("*"):
                 folder_sizes.get(current_folder, 0) + file_size
             )    
 
-            # Stop when reaching the repository root    
+            # Stop when reaching the repository root
+            if current_folder == REPO_PATH:
